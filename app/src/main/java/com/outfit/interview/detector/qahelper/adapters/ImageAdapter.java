bee801c49d5ba90e5ad4a1741c20947dc2d7d191
Dejan Ognjenović <dognjen@yandex.com>
2017-11-19 19:08:51 +0100

    Added business logic, manifest and other files.

---
+++ app/src/main/java/com/outfit/interview/detector/qahelper/adapters/ImageAdapter.java
@@ -0,0 +1,65 @@
+package com.outfit.interview.detector.qahelper.adapters;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Dejan on 19/11/2017.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence[] getAutofillOptions() {
+        return new CharSequence[0];
+    }
+
+
+
+
+}
