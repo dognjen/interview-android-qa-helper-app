3d580617453e2309c276ec4fb41eece48b9042a3
Dejan Ognjenović <dognjen@yandex.com>
2017-11-20 21:18:43 +0100

    Added java classes.

--- app/src/main/java/com/outfit/interview/detector/qahelper/MainActivity.java
+++ app/src/main/java/com/outfit/interview/detector/qahelper/MainActivity.java
@@ -1,13 +1,73 @@
 package com.outfit.interview.detector.qahelper;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.outfit.interview.detector.qahelper.adapters.ImageAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Bitmap> bitmapList;
+    private static int GALLERY_PICK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        GridView gridView = (GridView)findViewById(R.id.gridGallery);
+        ImageAdapter imageAdapter = new ImageAdapter(this, bitmapList);
+        gridView.setAdapter(imageAdapter);*/
+
+    }
+
+    public void readImages(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_PICK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+
+            if (requestCode == GALLERY_PICK && resultCode == RESULT_OK
+                    && null != data) {
+
+
+                Uri URI = data.getData();
+                String[] FILE = { android.provider.MediaStore.Images.Media.DATA };
+
+                Cursor cursor = getContentResolver().query(URI,
+                        FILE, null, null, null);
+
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(FILE[0]);
+                String img = cursor.getString(columnIndex);
+                cursor.close();
+
+                ImageView imgViewLoad = (ImageView) findViewById(R.id.imageView);
+                imgViewLoad.setImageBitmap(BitmapFactory
+                        .decodeFile(img));
+
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Please try again", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 }
--- app/src/main/java/com/outfit/interview/detector/qahelper/adapters/ImageAdapter.java
+++ app/src/main/java/com/outfit/interview/detector/qahelper/adapters/ImageAdapter.java
@@ -1,12 +1,18 @@
 package com.outfit.interview.detector.qahelper.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+
+import com.outfit.interview.detector.qahelper.R;
+
+import java.util.List;
 
 /**
  * Created by Dejan on 19/11/2017.
@@ -14,10 +20,12 @@ import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 
-    private Context mContext;
+    private Context context;
+    private List<Bitmap> bitmapList;
 
-    public ImageAdapter(Context c) {
-        mContext = c;
+    public ImageAdapter(Context context, List<Bitmap> bitmapList) {
+        this.context = context;
+        this.bitmapList = bitmapList;
     }
 
     @Override
@@ -37,19 +45,17 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
 
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+        Bitmap bitmap = bitmapList.get(position);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView = layoutInflater.inflate(R.layout.activity_gallery, null);
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        //ImageView imageView = (ImageView) convertView.findViewById(R.id.imageview_cover_art);
+
+        return convertView;
     }
 
 
