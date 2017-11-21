f118c6e434f959febf9bc12648e4d2b56ddc48c7
Dejan Ognjenović <dognjen@yandex.com>
2017-11-21 21:42:02 +0100

    Adde Util class.

---
+++ app/src/main/java/com/outfit/interview/detector/qahelper/utils/QAHelperUtils.java
@@ -0,0 +1,11 @@
+package com.outfit.interview.detector.qahelper.utils;
+
+/**
+ * Created by Dejan on 21/11/2017.
+ */
+
+public class QAHelperUtils {
+
+    public static final int GALLERY_IMG_PICK = 1;
+    public static final String GALLERY_TYPES = "image/*";
+}
